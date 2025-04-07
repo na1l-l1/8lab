a9b00a436170a0dd2e97dac80f4c85c5542592f4
Nail <nailbest001@gmail.com>
2025-04-07 14:47:20 +0500

    Start command

--- app/src/main/java/com/example/lab8/MyService.java
+++ app/src/main/java/com/example/lab8/MyService.java
@@ -1,14 +1,17 @@
 package com.example.lab8;
 
+import android.annotation.SuppressLint;
 
 import android.app.NotificationManager;
 import android.app.Service;
-
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
-
+import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+
 
 
 
@@ -35,4 +38,44 @@ public class MyService extends Service {
             Toast.makeText(this, "Ошибка: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    @SuppressLint("ForegroundServiceType")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: Запуск сервиса");
+
+        try {
+            createNotificationChannel();
+            startForeground(NOTIFICATION_ID, buildNotification());
+
+            Toast.makeText(this, "Сервер запущен", Toast.LENGTH_SHORT).show();
+
+            if (toneGenerator != null && !isPlaying) {
+                isPlaying = true;
+                Log.d(TAG, "onStartCommand: Запуск воспроизведения звука");
+
+                new Thread(() -> {
+                    while (isPlaying) {
+                        toneGenerator.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 1000);
+                        try {
+                            Thread.sleep(1100);
+                        } catch (InterruptedException e) {
+                            Log.d(TAG, "onStartCommand: Поток прерван", e);
+                            break;
+                        }
+                    }
+                }).start();
+            } else {
+                Log.w(TAG, "onStartCommand: ToneGenerator не готов или уже воспроизводит звук");
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "onStartCommand: Ошибка запуска сервиса", e);
+            Toast.makeText(this, "Ошибка: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            stopSelf();
+        }
+
+        return START_STICKY;
     }
+}
